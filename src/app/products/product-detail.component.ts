134c664985c6acca5ffb7fdf7cf3b7ae02b349a6
TEC10101 <tyler.corum@gmail.com>
2020-02-03 18:01:30 -0800

    before modding the navbar

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: 'pm-product-detail', // only needed if nested in another module
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
