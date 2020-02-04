2f7361d0a4efaa8579e48d5c45ed5eea767b785f
TEC10101 <tyler.corum@gmail.com>
2020-02-03 19:02:15 -0800

    guard setup

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
+
+import { IProduct } from './product';
 
 @Component({
   // selector: 'pm-product-detail', // only needed if nested in another module
@@ -7,13 +10,29 @@ import { ActivatedRoute } from "@angular/router";
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
 
-  constructor(private _route: ActivatedRoute) { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) { }
 
   ngOnInit() {
     let id = +this._route.snapshot.paramMap.get('id'); // use the snapshot approach only if you need the initial value of the parameter
     
     // code to retrieve the product info
+    this.pageTitle += `: ${id}  (Demo Product Below)`;
+    this.product =  {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2019",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.png"
+    }
   }
-
+  onBack(): void {
+    this._router.navigate(['/products'])
+  }
 }
