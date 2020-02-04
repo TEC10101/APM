68ba1875af0470204e02976d7a0507bdde9d620b
TEC10101 <tyler.corum@gmail.com>
2020-02-03 18:12:36 -0800

    successful routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root', // convention is to prefix id with something to id as part of our app
   template: `
-  <div><h1>{{pageTitle}}</h1>
-  <pm-products></pm-products>
-</div>
+  <nav class='navbar navbar-expand navbar-light bg-light'>
+    <a class='navbar-brand'>{{pageTitle}}</a>
+    <ul class='nav nav-pills'>
+      <li><a class='nav-link' [routerLink]="['/welcome']">Home</a></li>
+      <li><a class='nav-link' [routerLink]="['/products']">Product List</a></li>
+    </ul>
+  </nav>
+  <div class='container'>
+    <router-outlet></router-outlet>
+  </div>
 `
+//       <li><a [routerLink]="['/welcome']">Home</a></li>
+//<li><a [routerLink]="['/products']">Product List</a></li>
 })
 export class AppComponent {
   pageTitle: string = '02 Angular Getting Started';
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -3,7 +3,7 @@ import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-  selector: 'pm-products', // you can use the selector as a directive in other component templates
+  // selector: 'pm-products', // you can use the selector as a directive in other component templates
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
   providers: [ProductService]
